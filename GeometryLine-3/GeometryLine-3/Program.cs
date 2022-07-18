f3f409fa8126b2140db81ccb227add1d589cad35
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-18 22:38:35 +0530

    Added geometry problem 3 File

---
+++ GeometryLine-3/GeometryLine-3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter points for first line");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+double l1 = Math.Pow((x2 - x1), 2);
+double l2 = Math.Pow((y2 - y1), 2);
+double length = l1 + l2;
+double line_lenght1 = Math.Sqrt(length);
+//Console.WriteLine("Length of line 1 is " + line_lenght1);
+Console.WriteLine("Enter points for second line");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int a2 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+double m1 = Math.Pow((a2 - a1), 2);
+double m2 = Math.Pow((b2 - b1), 2);
+double length2 = m1 + m2;
+double line_lenght2 = Math.Sqrt(length2);
+//Console.WriteLine("Length of line 2 is " + line_lenght2);
+if (line_lenght1 == line_lenght2)
+    Console.WriteLine("Two Lines are of Equal Length");
+else if (line_lenght1 > line_lenght2)
+    Console.WriteLine("Lines 1 is larger then Line 2");
+else
+    Console.WriteLine("Line 2 is Larger then line 1");
